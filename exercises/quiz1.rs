8127c37a4fa6ab1e6c08c6084fe502bd9d7a5da1
bws428 <briandwendt@gmail.com>
2023-11-14 07:31:28 -0500

    complete quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -16,7 +16,15 @@
 // I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(quantity: i32) -> i32 {
+    if quantity <= 40 {
+        // An apple costs 2 rustbucks.
+        return 2 * quantity;
+    } else {
+        // An apple costs 1 rustbuck.
+        return quantity;
+    }
+}
 
 // Don't modify this function!
 #[test]
